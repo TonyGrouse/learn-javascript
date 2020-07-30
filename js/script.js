e5b26c2eb277fcc8a5c71a7dc106a1221f1ecd5b
Anton <ryabchikov.anton.20@mail.ru>
2020-07-30 14:12:01 +0300

    Practice №4

--- js/script.js
+++ js/script.js
@@ -1,16 +1,108 @@
 "use strict";
 
+// Практика №4 - консольное приложение
 
-const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
+    start: function () {
+        personalMovieDB.count = +prompt("Сколько фильмов вы посмотрели?", "");
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("Сколько фильмов вы посмотрели?", "");
+        }
+    },
+    detectPersonalLevel: function () {
+        if (personalMovieDB.count <= 10 && personalMovieDB.count >= 0) {
+            alert("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count <= 30 && personalMovieDB.count > 10) {
+            alert("Вы классический зритель");
+        } else if (personalMovieDB.count > 30) {
+            alert("Вы киноман");
+        }
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt("Один из последних просмотренных фильмов?", ""),
+                b = prompt("На сколько оцените его?", "");
+    
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            } 
+        }
+    },
+    showMyDB: function () {
+        const privatAnsw = +prompt("Вы хотите чтобы ваши фильмы кто-то видел?(1 - да, 2 - нет)");
+        if (privatAnsw == 2) {
+            personalMovieDB.privat = true;
+        }else {
+            personalMovieDB.privat = false;
+        }
+        if (personalMovieDB.privat == false) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function () {
+        for (let i = 1; i < 2; i++){
+            let genres = prompt(`Введите ваши любимые жанры через запятую:`).toLowerCase();
+            
+            if (genres == '' || genres == null) {
+                i--;
+            } else{
+                personalMovieDB.genres = genres.split(', ');
+                personalMovieDB.genres.sort();
+            }
+        }
+        personalMovieDB.genres.forEach ((name, num) => {
+            console.log(`Любимый жанр ${num + 1} - это ${name}`);
+        });
+    },
 };
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> master
+// personalMovieDB.start();
+// personalMovieDB.detectPersonalLevel();
+// personalMovieDB.rememberMyFilms();
+// personalMovieDB.showMyDB();
+// personalMovieDB.writeYourGenres();
+<<<<<<< HEAD
+=======
+=======
 
+<<<<<<< HEAD
+if (personalMovieDB.count <= 10 && personalMovieDB.count >= 0) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count <= 30 && personalMovieDB.count > 10) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} else {
+    console.log("ERROR");
+}
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    } 
+}
+=======
 let a = prompt("Один из последних просмотренных фильмов?", ""),
     b = prompt("На сколько оцените его?", ""),
     c = prompt("Один из последних просмотренных фильмов?", ""),
@@ -18,38 +110,8 @@ let a = prompt("Один из последних просмотренных фи
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
+>>>>>>> master
+>>>>>>> master
 
 console.log(personalMovieDB);
-
-
-
-
-
-
-
-
-
-
-
-    /* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
+>>>>>>> master
---
+++ js/theory.js
@@ -0,0 +1,525 @@
+"use strict";
+
+// if (4 == 3) {
+//     console.log("Yes");
+// }else {
+//     console.log("No");
+// }
+
+
+// const num = 59;
+
+// // (num == 6) ? console.log('True') : console.log('False');
+
+// switch (num) {
+//     case 49:
+//         console.log("No");
+//         break;
+//     case 59:
+//         console.log("No");
+//         break;
+//     case 50:
+//         console.log("Yes");
+//         break;
+//     default:
+//         console.log("Sorry");
+//         break;
+// }
+
+
+// let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }while (num < 55);
+
+// for (let num = 50; num <= 60; num++) {
+//     if (num === 55) {
+//         continue;
+//     }else if (num === 58) {
+//         break;
+//     }
+//     console.log(num);
+// }
+
+// let num = 10;
+//* function declaration
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 30;
+//     console.log(num);
+// }
+
+// showFirstMessage("Hello World!");
+// console.log(num);
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+// let a = calc(5, 21);
+// console.log(a);
+
+// function ret() {
+//     let num = 51;
+//     if (num == 50) {
+//         num = num / 2;
+//     }else {
+//         num = num * 2;
+//     }
+//     return num;
+// }
+
+// let anotherNum = ret();
+// console.log(anotherNum);
+
+//* function expression
+// const logger = function() {
+//     console.log("hello");
+// };
+
+// logger();
+
+//* стрелочные функции
+// const calculate = (a,b) => {
+//     return a + b;
+// };
+
+// console.log(calculate(2,3));
+
+function calculator() {
+<<<<<<< HEAD
+    let greet = prompt("Добро пожаловать в импровизированный калькулятор!\n"  +
+                       "Нажмите +, чтобы сложить;\n" + 
+                       "-, чтобы вычесть;\n" +
+                       "*, чтобы умножить;\n" +
+                       "/, чтобы поделить;\n" +
+                       "**, чтобы возвести в степень;", "");
+=======
+    let greet = prompt("Добро пожаловать в импровизированный калькулятор!"  +
+                       " Нажмите +, чтобы сложить;" + 
+                       " -, чтобы вычесть;" +
+                       " *, чтобы умножить;" +
+                       " /, чтобы поделить;" +
+                       " **, чтобы возвести в степень;", "");
+>>>>>>> master
+
+    const multiply = function () {
+        alert("Введите два числа чтобы умножить.");
+        let a = +prompt("Первое число:", ""),
+            b = +prompt("Второе число:", ""),
+            c = a * b;
+        alert("Ваш результат: " + c);
+    };
+    const divide = function () {
+        alert("Введите два числа чтобы разделить.");
+        let a = +prompt("Первое число:", ""),
+            b = +prompt("Второе число:", ""),
+            c = a / b;
+        alert("Ваш результат: " + c);
+    };
+    const fold = function () {
+        alert("Введите два числа чтобы сложить.");
+        let a = +prompt("Первое число:", ""),
+            b = +prompt("Второе число:", ""),
+            c = a + b;
+        alert("Ваш результат: " + c);
+    };
+    const deduct = function () {
+        alert("Введите два числа чтобы вычесть.");
+        let a = +prompt("Первое число:", ""),
+            b = +prompt("Второе число:", ""),
+            c = a - b;
+        alert("Ваш результат: " + c);
+    };
+    const stage = function () {
+        alert("Введите два числа чтобы возвести в степень.");
+        let a = +prompt("Возводимое число:", ""),
+            b = +prompt("Степень:", ""),
+            c = a ** b;
+        alert("Ваш результат: " + c);
+    };
+    for (let i = 0; i < 1; i++) {
+        if (greet == "*") {
+            multiply();
+        }else if (greet == "/") {
+            divide();
+        }else if (greet == "+") {
+            fold();
+        }else if (greet == "-") {
+            deduct();
+        }
+        else if (greet == "**") {
+            stage();
+        }
+        let yet = +prompt("Хотите еще что-то посчитать?(1 - да, 2 - нет)");
+        if (yet == 1) {
+            greet = prompt("Выберите знак: *, /, +, -");
+            i--;
+        }else {
+            alert("Пака лалка(.Y.)");
+            i = 1;
+        }
+    }
+}
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+
+//* callback функции
+// function learnJS(lang, callback) {
+//     console.log(`Я учу ${lang}`);
+//     callback();
+// }
+
+// function done() {
+//     console.log("Я прошел этот урок!"); 
+// }
+
+// learnJS('JavaScript', done);
+
+
+//* объекты, деструктуризация объектов
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red',
+//     },
+//     // создадим метод
+//     makeTest: function () {
+//         console.log('test');
+//     }
+// };
+// вызовем метод
+// options.makeTest();
+
+// деструктурируем элементы
+// const {border, bg} = options.colors;// вытащили 2 ключа из объекта colors
+// console.log(border);// выводим значение ключа border
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//         }
+//     }else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     }
+// }
+// console.log(Object.keys(options).length);
+
+
+//* массивы и псевдомассивы
+// const arr = [1, 44, 8, 55, 6, 12];
+
+// arr.pop();
+// console.log(arr);
+// arr.push('afsasdfgds');
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// arr.forEach(function (item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// console.log(products.join("; "));
+// console.log(arr.sort(sortedNums));
+
+// function sortedNums(a, b) {
+//     return a - b;
+// }
+
+
+//* передача по ссылке или по значению, оператор Spread
+// function copy (mainObj) {
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 4,
+//     b: 7,
+//     c: 33,
+//     d: {
+//         x: 12,
+//         y: 22,
+//     },
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 90;
+
+// console.log(numbers);
+// console.log(newNumbers);
+
+// const add = {
+//     g: 11,
+//     e: 13,
+// };
+
+// console.log(Object.assign(numbers, add));
+
+// const oldArr = ['a', 'b', 'c'];
+// const newArr = oldArr.slice();
+// newArr[1] = 'sdfs';
+
+// console.log(newArr);
+// console.log(oldArr);
+
+// const video = ['youtube', 'twich', 'rutube'],
+//       blogs = ['wordpress', 'livejournal', 'blogger'],
+//       internet = [...video, ...blogs, 'vk', 'telegramm'];
+
+// console.log(internet);
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 7, 34];
+// log(...num);
+
+// const object = {
+//     one: 1,
+//     two: 2,
+// };
+// const newObject = {...object};
+// newObject.one = 3;
+// console.log(object);
+// console.log(newObject);
+
+
+//* ООП и прототипно-ориентированное наследование
+// const soldier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function() {
+//         console.log('Hello');
+//     },
+// };
+
+// const john = Object.create(soldier);
+
+// const john = {
+//     health: 100,
+// };
+
+// john.health = 100;
+// john.skill = 500;
+
+// john.__proto__ = soldier;
+// Object.setPrototypeOf(john, soldier);
+
+// console.log(john);
+// console.log(john.armor);
+// john.sayHello();
+=======
+calculator();
+>>>>>>> master
+
+//* callback функции
+// function learnJS(lang, callback) {
+//     console.log(`Я учу ${lang}`);
+//     callback();
+// }
+
+// function done() {
+//     console.log("Я прошел этот урок!"); 
+// }
+
+// learnJS('JavaScript', done);
+
+
+//* объекты, деструктуризация объектов
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red',
+//     },
+//     // создадим метод
+//     makeTest: function () {
+//         console.log('test');
+//     }
+// };
+// вызовем метод
+// options.makeTest();
+
+// деструктурируем элементы
+// const {border, bg} = options.colors;// вытащили 2 ключа из объекта colors
+// console.log(border);// выводим значение ключа border
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//         }
+//     }else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     }
+// }
+// console.log(Object.keys(options).length);
+
+
+//* массивы и псевдомассивы
+// const arr = [1, 44, 8, 55, 6, 12];
+
+// arr.pop();
+// console.log(arr);
+// arr.push('afsasdfgds');
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// arr.forEach(function (item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// console.log(products.join("; "));
+// console.log(arr.sort(sortedNums));
+
+// function sortedNums(a, b) {
+//     return a - b;
+// }
+
+
+//* передача по ссылке или по значению, оператор Spread
+// function copy (mainObj) {
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 4,
+//     b: 7,
+//     c: 33,
+//     d: {
+//         x: 12,
+//         y: 22,
+//     },
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 90;
+
+// console.log(numbers);
+// console.log(newNumbers);
+
+// const add = {
+//     g: 11,
+//     e: 13,
+// };
+
+// console.log(Object.assign(numbers, add));
+
+// const oldArr = ['a', 'b', 'c'];
+// const newArr = oldArr.slice();
+// newArr[1] = 'sdfs';
+
+// console.log(newArr);
+// console.log(oldArr);
+
+// const video = ['youtube', 'twich', 'rutube'],
+//       blogs = ['wordpress', 'livejournal', 'blogger'],
+//       internet = [...video, ...blogs, 'vk', 'telegramm'];
+
+// console.log(internet);
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 7, 34];
+// log(...num);
+
+// const object = {
+//     one: 1,
+//     two: 2,
+// };
+// const newObject = {...object};
+// newObject.one = 3;
+// console.log(object);
+// console.log(newObject);
+
+
+//* ООП и прототипно-ориентированное наследование
+// const soldier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function() {
+//         console.log('Hello');
+//     },
+// };
+
+// const john = Object.create(soldier);
+
+// const john = {
+//     health: 100,
+// };
+
+// john.health = 100;
+// john.skill = 500;
+
+// john.__proto__ = soldier;
+// Object.setPrototypeOf(john, soldier);
+
+<<<<<<< HEAD
+// console.log(john);
+// console.log(john.armor);
+// john.sayHello();
+=======
+>>>>>>> master
+>>>>>>> master
